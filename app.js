9984840f4e463f25acab64ff53e8600e3af5ae4a
dlipahir <77920752+dlipahir@users.noreply.github.com>
2021-12-22 16:06:44 +0530

    update

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-function myfun(e) {
-    let cls = "m" + e.text;
-    console.log(cls);
-    let chk = document.querySelector(".otherpages");
-    for (let item of chk.children) {
-        if (item.classList.contains("shifttome")) { item.classList.toggle("shifttome"); }
-    }
-    document.getElementsByClassName(cls)[0].classList.toggle("shifttome");
-
-}
-let itm = document.querySelector(".otherpages");
-// window.addEventListener("wheel",fun2(e));
-window.onwheel= fun2;
-let cnt=0;
-function fun2(e){
-   // console.log(1234);
-    
-    cnt+=e.deltaY;
-    console.log(cnt);
-    if(cnt<200){itm.children[0].classList.add("shifttome");}
-    else if(cnt<500){ itm.children[1].classList.add("shifttome");}
-   else if(cnt<800) {itm.children[2].classList.add("shifttome");}
-   else if(cnt<1000){ itm.children[3].classList.add("shifttome");}
-   //if(cnt>1000)
-  // else cnt=0;
-
-
-
-
-
-
-}
-// window.addEventListener('wheel',(event) => {
-//     console.log('Scrolling...');
+let navbar=document.querySelector('.navbar');
+// window.addEventListener("click",function(){
+//     if(navbar.style.width==="150px")
+//     navbar.style.width="60px";
 // });
+document.addEventListener('DOMContentLoaded',function(){
+    let wrapper=document.querySelector('#wrapper');
+    let toplayer =document.querySelector('.top');
+    let handle=document.querySelector('.handle');
+    let skew=0;
+    let delta=0;
+     if(wrapper.className.indexOf('skewed') != -1){ 
+        skew=10000;}
+      wrapper.addEventListener('mousemove', function(e){
+            delta = (e.clientX - window.innerWidth/2) * 0.5;
+            handle.style.left= e.clientX + delta + 'px';
+            toplayer.style.width= e.clientX+skew+delta+'px';
+          
+      });
+});
